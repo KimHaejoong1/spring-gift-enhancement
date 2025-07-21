6d782959c3571b7e47ef1fd7b41ce3db1f599540
KimHaejoong1 <hjkim2124@gmail.com>
2025-07-21 21:21:52 +0900

    feat(option): Option 관련 API 구현

---
+++ src/main/java/gift/repository/OptionRepository.java
@@ -0,0 +1,11 @@
+package gift.repository;
+
+import gift.entity.Option;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OptionRepository extends JpaRepository<Option, Integer> {
+    List<Option> findByProductId(Integer productId);
+    boolean existsByProductIdAndName(Integer productId, String name);
+}

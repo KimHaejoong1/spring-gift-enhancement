76bdd3c54ec4764f471f9e9e1b7e3ae8fa0c425e
KimHaejoong1 <hjkim2124@gmail.com>
2025-07-22 01:52:33 +0900

    perf(repository): Wishlist 페이지네이션 N+1 문제 해결

--- src/main/java/gift/repository/WishlistRepository.java
+++ src/main/java/gift/repository/WishlistRepository.java
@@ -5,6 +5,7 @@ import gift.entity.Product;
 import gift.entity.WishList;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,6 +15,7 @@ import java.util.Optional;
 @Repository
 public interface WishlistRepository extends JpaRepository<WishList, Integer> {
     List<WishList> findByMember(Member member);
+    @EntityGraph(attributePaths = {"product"})
     Page<WishList> findByMember(Member member, Pageable pageable);
     Optional<WishList> findByMemberAndProduct(Member member, Product product);
 }
